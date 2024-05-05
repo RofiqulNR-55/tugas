b55f70991143ddf557580c2688a6969e3dfd7705
RofiqulNR-55 <wrofiqul@gmail.com>
2024-05-05 21:02:58 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Flutter Developer'],
+    strings: ['Web Development'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
